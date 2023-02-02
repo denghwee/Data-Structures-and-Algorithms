956936773101d6610f5be6bf4908011151e4654f
Nguyễn Đăng Huy <111379758+Chimmeyhere@users.noreply.github.com>
2023-02-02 16:28:27 +0700

    Update DSA01003.cpp

--- DSA01003.cpp
+++ DSA01003.cpp
@@ -2,25 +2,34 @@
 using namespace std;
 #define faster(); ios_base::sync_with_stdio(false);cin.tie();cout.tie();
 
+void next_Permu(vector <long long> &a, int n) {
+    for (int i = n - 2 ; i >= 0 ; i--)
+    {
+        if ( a[i] < a[i+1] )
+        {
+            sort(a.begin()+i,a.end());
+            swap(a[i],a[i+1]);
+            break;
+        }
+    }
+}
+
 int main() {
-    faster();
     int t;
     cin >> t;
     while ( t-- )
     {
         int n;
         cin >> n;
-        string s;
+        vector <long long> a(n);
         for (int i = 0 ; i < n ; i++)
         {
-            int x;
-            cin >> x;
-            s += to_string(x);
-        }
-        next_permutation(s.begin(),s.end());
+            cin >> a[i];
+        }   
+        next_Permu(a,n);
         for (int i = 0 ; i < n ; i++)
         {
-            cout << s[i] << " ";
+            cout << a[i] << " ";
         }
         if ( t != 0 )
         {
