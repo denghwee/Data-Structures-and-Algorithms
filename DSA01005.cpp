60ca2b4b135824ef90c940b489f0534393b6663d
Nguyễn Đăng Huy <111379758+Chimmeyhere@users.noreply.github.com>
2023-02-02 17:07:54 +0700

    Create DSA01005.cpp

---
+++ DSA01005.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define faster(); ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+
+void initiate(vector <long long> &a, int n) {
+    for (int i = 0 ; i < n ; i++)
+    {
+        a[i] = i + 1;
+    }
+}
+
+void print_Out(vector <long long> a, int n) {
+    for (int i = 0 ; i < n ; i++)
+    {
+        cout << a[i];
+    }
+    cout << " ";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while ( t-- )
+    {
+        int n;
+        cin >> n;
+        vector <long long> a(n);
+        initiate(a,n);
+        do
+        {
+            print_Out(a,n);
+        }
+        while ( next_permutation(a.begin(),a.end()) );
+        
+        if ( t != 0 )
+        {
+            cout << endl;
+        }
+    }
+}
