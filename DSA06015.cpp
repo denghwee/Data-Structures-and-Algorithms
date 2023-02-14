5af8b7c6901a36b8df68f325335fdf0a0c0a4693
Nguyễn Đăng Huy <111379758+Chimmeyhere@users.noreply.github.com>
2023-02-14 17:19:46 +0700

    Create DSA06015.cpp

---
+++ DSA06015.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define faster(); ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define matrix vector <vector<long long>>
+#define MOD 1000000007
+#define el "\n"
+const long long big = 1e6;
+
+int main() {
+    faster();
+    int t;
+	cin >> t;
+	while ( t-- )
+	{	
+		int n;
+		cin >> n;
+		long long a[n];
+		for (int i = 0 ; i < n ; i++)
+		{
+			cin >> a[i];
+		}
+		sort(a,a+n);
+		for (int i = 0 ; i < n ; i++)
+		{
+			cout << a[i] << " ";
+		}
+		if ( t != 0 )
+		{
+			cout << endl;
+		}
+	}
+}
