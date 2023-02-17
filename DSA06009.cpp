5f63daf3b8b8c07ffa31cf8ce1299dad80cfdec3
Nguyễn Đăng Huy <111379758+Chimmeyhere@users.noreply.github.com>
2023-02-17 17:54:35 +0700

    Create DSA06009.cpp

---
+++ DSA06009.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define faster(); ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define matrix vector <vector<long long>>
+#define MOD 1000000007
+#define el "\n"
+#define fi first
+#define se second
+const long long big = 1e6;
+
+int main() {
+	faster();
+	int t;
+	cin >> t;
+	while ( t-- )
+	{
+		int n, k, x, res = 0;
+		cin >> n >> k;
+		map <int,int> m;
+		for (int i = 0 ; i < n ; i++)
+		{
+			cin >> x;
+			m[x]++;
+		}
+		for (auto i : m)
+		{
+			if ( i.first * 2 == k )
+			{
+				res += i.second * (i.second - 1);
+			}	
+			else
+			{
+				res += i.second * m[k - i.first];
+			}
+		}
+		cout << res / 2;
+		if ( t != 0 )
+		{
+			cout << endl;
+		}
+	}
+}
