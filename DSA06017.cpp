22886c47490ab09e720b6fc7ff7a50c7120dae7f
Nguyễn Đăng Huy <111379758+Chimmeyhere@users.noreply.github.com>
2023-02-17 18:42:08 +0700

    Update DSA06017.cpp

--- DSA06017.cpp
+++ DSA06017.cpp
@@ -16,27 +16,16 @@ int main() {
 	{
 		int m, n;
 		cin >> m >> n;
-		vector <long long> a, b, c, d;
-		a.assign(m,0);
-		b.assign(n,0);
-		c.assign(100005,0);
-		d.assign(100005,0);
-		for (int i = 0 ; i < m ; i++)
+		vector <long long> a;
+		a.assign(m+n,0);
+		for (int i = 0 ; i < m + n ; i++)
 		{
 			cin >> a[i];
-			c[a[i]]++;
 		}
-		for (int i = 0 ; i < n ; i++)
+		sort(a.begin(),a.end());
+		for (int i = 0 ; i < m + n ; i++)
 		{
-			cin >> b[i];
-			d[b[i]]++;
-		}
-		for (int i = 0 ; i <= 100000 ; i++)
-		{
-			if ( c[i] != 0 || d[i] != 0 )
-			{
-				cout << i << " ";
-			}
+			cout << a[i] << " ";
 		}
 		if ( t != 0 )
 		{
