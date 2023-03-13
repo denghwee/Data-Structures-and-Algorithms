1c6a3b1d63e191945d148e8af29d4ae8c6557755
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-03-13 14:43:55 +0700

    Create DSA04010.cpp

---
+++ DSA04010.cpp
@@ -0,0 +1,33 @@
+//I'm HuyNho or you can call me Chimmey
+#include <bits/stdc++.h>
+using namespace std;
+#define faster() ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define matrix vector <vector<long long>>
+#define MOD 1000000007
+#define el "\n"
+const long long big = 1e6;
+
+int main() {
+	int t, test = 0;
+	cin >> t;
+	while ( t-- )
+	{
+		int n;
+        cin >> n;
+        int x, sum = 0, res = 0;
+        for (int i = 0 ; i < n ; i++)
+        {
+            cin >> x;
+            sum += x;
+            res = max(res,sum);
+            if ( sum < 0 )
+            {
+                sum = 0;
+            }
+        }
+        cout << res;
+		{
+			cout << el;
+		}
+	}
+}
