443fa638cac5840e50ac2820a00636a6779f4179
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-03-13 15:15:34 +0700

    Create DSA04016.cpp

---
+++ DSA04016.cpp
@@ -0,0 +1,30 @@
+//I'm HuyNho or you can call me DengHwee
+#include <bits/stdc++.h>
+using namespace std;
+#define faster() ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define matrix vector <vector<long long>>
+#define MOD 1000000007
+#define el "\n"
+const long long big = 1e6;
+
+int main() {
+    faster();
+	int t;
+	cin >> t;
+	while ( t-- )
+	{
+		long long m, n, k;
+        cin >> m >> n >> k;
+        vector <long long> a(m+n);
+        for (int i = 0 ; i < m + n ; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(),a.end());
+        cout << a[k-1];
+        if ( t != 0 )
+		{
+			cout << el;
+		}
+	}
+}
